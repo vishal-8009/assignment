1da794ad370c40cd30efd06806eed2b3179cd253
vishal-8009 <vishalji8009@gmail.com>
2025-08-18 09:48:21 +0530

    abcd

---
+++ abcd.py
@@ -0,0 +1,14 @@
+n=int(input("Enter a Row:"))
+for row in range(n):
+    for col in range(1,(n-row+1)):
+        print(chr(col+64), end="")
+    for col in range(1,row+1):
+        print(" ", end="") 
+    for col in range(2,row+1):
+            print(" ", end="")
+    for col in range(n-row,0,-1):
+        if col==n:
+            continue
+        print(chr(col+64),end="")
+    print()
+        
